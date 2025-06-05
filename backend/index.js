1b25a05e5eb6dd0606fb075bbc72f76f90020a00
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-06-04 21:10:21 -0500

    Created database and User Schema init

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 import express from "express"
+import { connectDB } from "./db/connectDB.js"
+import dotenv from 'dotenv';
+import authRoutes from "./routes/authRoutes.js"
+
+dotenv.config();
 
 const app = express()
+
+app.use("/api/auth", authRoutes)
+
+app.listen(3000, () => {
+    connectDB()
+    console.log(`Server is running on Port 3000`)
+})
 
