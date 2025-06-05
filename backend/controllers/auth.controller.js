ab50fe4ade737019e653d3badc55d3825a6b8053
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-06-04 21:40:27 -0500

    signup route init

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,35 @@
+import { User } from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js"
+
 export const signup = async (req, res) => {
-    res.send("sign up route")
+    const {email, password, name} = req.body
+    try {
+        if (!email || !password || !name) {
+            throw new Error("All fields are required")
+        }
+
+        const userAlreadyExists = await User.findOne({email})
+        if (userAlreadyExists) {
+            return res.status(400).json({message: "User Already Exists!"})
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10)
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 hours
+        })
+        await user.save();
+
+        generateTokenAndSetCookie(res, user._id)
+
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 }
 
 export const login = async (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,17 @@ import express from "express"
 import { connectDB } from "./db/connectDB.js"
 import dotenv from 'dotenv';
 import authRoutes from "./routes/authRoutes.js"
-
 dotenv.config();
 
 const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes)
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     connectDB()
-    console.log(`Server is running on Port 3000`)
+    console.log(`Server is running on port ${PORT}`)
 })
 
